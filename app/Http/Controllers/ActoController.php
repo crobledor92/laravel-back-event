fad3168478012498e67aa52a82a7be32108d954c
hrubiogi <hrubiogi@uoc.edu>
2023-12-21 19:47:04 +0100

    se eliminan cambios de add_acto

--- app/Http/Controllers/ActoController.php
+++ app/Http/Controllers/ActoController.php
@@ -23,29 +23,3 @@ class ActoController extends Controller {
         return view('personal-panel',['actos' => $actos,'inscritos' => $inscritos]);
     }
 }
-
-public function AddActo(Request $request)
-{
-    // Validaciones
-    $request->validate([
-        'fecha' => 'required|date',
-        'hora' => 'required|date_format:H:i', // Validación para el formato de hora (HH:mm)
-        'titulo' => 'required|string',
-        'resumen' => 'required|string',
-        'descripcion' => 'required|string',
-        'asistentes' => 'required|integer',
-        'tipoActo' => 'required', // Ajusta según el tipo de datos que esperas para tipoActo
-        'personasId' => 'required|array', // Asegúrate de que sea un array
-        'personasId.*' => 'exists:personas,Id_persona', // Asegúrate de que las personas existan en la tabla personas
-    ]);
-
-    // Crear un nuevo acto
-    $acto = new Acto;
-    $acto->fecha = $request->input('fecha');
-    // Asignar otros campos
-
-    // Guardar en la base de datos
-    $acto->save();
-
-    return redirect('/actos')->with('success', 'Acto añadido correctamente');
-}
